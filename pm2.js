bb304375d4b79b5d609fa442f73c23538e61d89a
yangchigi <yangchigi@yangchigi.com>
2019-08-07 15:30:43 +0900

    Create pm2.js

---
+++ pm2.js
@@ -0,0 +1,17 @@
+{
+    "apps": [
+        {
+            "/**/ ": "mjpeg",
+            "name": "streamer",
+            "script": ".mjpg_streamer",
+            "exec_mode": "fork_mode",
+            "args": [
+                "-i",
+                "input_uvc.so -d /dev/video0 -n -f 15 -r 1280x720",
+                "-o",
+                "output_http.so -w /usr/share/mjpg_streamer/www  -p 8080"
+            ],
+            "log_date_format": "YYYY-MM-DD HH:mm:ss"
+        }q
+    ]
+}
