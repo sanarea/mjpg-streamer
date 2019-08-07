98a7d4617706db09c86051f6c37663e9d3c464ea
yangchigi <yangchigi@yangchigi.com>
2019-08-07 15:34:06 +0900

    Update pm2.js

--- pm2.js
+++ pm2.js
@@ -3,7 +3,7 @@
         {
             "/**/ ": "mjpeg",
             "name": "streamer",
-            "script": ".mjpg_streamer",
+            "script": "mjpg_streamer",
             "exec_mode": "fork_mode",
             "args": [
                 "-i",
@@ -12,6 +12,6 @@
                 "output_http.so -w /usr/share/mjpg_streamer/www  -p 8080"
             ],
             "log_date_format": "YYYY-MM-DD HH:mm:ss"
-        }q
+        }
     ]
 }
